a6f413f838592442bc6c8e694ffcf5bf01bf4594
NicolasBrazzo <nicolasbrazzo8@gmail.com>
2026-03-13 19:20:06 +0100

    fix server.js

--- server/server.js
+++ server/server.js
@@ -44,6 +44,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Internal server error" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend ON at port ${PORT}`);
 });
